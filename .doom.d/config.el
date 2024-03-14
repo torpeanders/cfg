f5d61afa0fa90afbf4682788d4f33ea61718e051
Anders Rønningen <anders.ronningen@neat.no>
2024-03-14 09:08:40 +0100

    doom: use default font and theme

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'vscode-dark-plus)
+;(setq doom-theme 'vscode-dark-plus)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -90,7 +90,7 @@
 (setq confirm-kill-emacs nil)
 
 ;; looks are important
-(setq doom-font (font-spec :family "Source Code Pro" :size 13))
+; (setq doom-font (font-spec :family "Source Code Pro" :size 13))
 
 ;; some magit customization
 (setq magit-display-buffer-function
