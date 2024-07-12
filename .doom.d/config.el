45667d2fe6db9c78cbe5f85dcc796c7ebc972097
Anders Rønningen <anders.ronningen@neat.no>
2024-07-12 08:43:12 +0200

    doom: cosmetics and no line numbers

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -33,7 +33,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type nil)
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
@@ -170,28 +170,28 @@
 
 ;; org setup
 (after! org
-  (setq org-tags-column -80)
-  (setq
-   org-todo-keywords
-   '((sequence "TODO(t)" "INPROGRESS(i@)" "|" "DONE(f@)" "DELEGATED(d@)" "CANCELLED(c@)"))
-   org-todo-keyword-faces
-   '(("TODO" . org-warning)
-     ("DONE" . (:foreground "green" :weight bold))
-     ("DELEGATED" . (:foreground "yellow" :weight bold))
-     ("CANCELLED" . (:foreground "red" :weight bold))
-     ))
-   ;; ... other org configuration here
-)
+        (setq org-tags-column -80)
+        (setq
+         org-todo-keywords
+         '((sequence "TODO(t)" "INPROGRESS(i@)" "|" "DONE(f@)" "DELEGATED(d@)" "CANCELLED(c@)"))
+         org-todo-keyword-faces
+         '(("TODO" . org-warning)
+           ("DONE" . (:foreground "green" :weight bold))
+           ("DELEGATED" . (:foreground "yellow" :weight bold))
+           ("CANCELLED" . (:foreground "red" :weight bold))
+           ))
+        ;; ... other org configuration here
+        )
 
 ;; magit setup
 (after! magit
-  (set-default 'magit-stage-all-confirm nil)
-  (set-default 'magit-unstage-all-confirm nil)
-  (setq magit-ediff-dwim-show-on-hunks t)
-  (setq ediff-window-setup-function 'ediff-setup-windows-plain)
-  (setq ediff-split-window-function 'split-window-horizontally)
-  (setq magit-bury-buffer-function #'magit-restore-window-configuration)
-  )
+        (set-default 'magit-stage-all-confirm nil)
+        (set-default 'magit-unstage-all-confirm nil)
+        (setq magit-ediff-dwim-show-on-hunks t)
+        (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+        (setq ediff-split-window-function 'split-window-horizontally)
+        (setq magit-bury-buffer-function #'magit-restore-window-configuration)
+        )
 
 ;; diff init.el to look for new and cool stuff
 (defun anr/ediff-init-and-example ()
@@ -201,12 +201,12 @@
                (concat doom-private-dir "init.el")))
 
 (define-key! help-map
-  "di"   #'doom/ediff-init-and-example
-  )
+             "di"   #'doom/ediff-init-and-example
+             )
 
 ;; show workspace in modeline
 (after! doom-modeline
-  (setq doom-modeline-persp-name t))
+        (setq doom-modeline-persp-name t))
 
 ;; no new workspace on project switch
 (setq +workspaces-on-switch-project-behavior nil)
@@ -228,7 +228,7 @@
 
 ;; alternate whitespace-mode with whitespace.el defaults, doom defaults and off:
 (defun anr/set-whitespace-defaults()
-  ; only save the values the first time we get here
+                                        ; only save the values the first time we get here
   (unless (boundp 'anr/default-whitespace-style)
     (setq anr/default-whitespace-style                (default-value 'whitespace-style)
           anr/default-whitespace-display-mappings     (default-value 'whitespace-display-mappings)
@@ -236,16 +236,16 @@
           anr/doom-whitespace-display-mappings        whitespace-display-mappings
           anr/whitespace-mode                         "doom")))
 
-; whitespace-style etc are set up with default-values in whitespace.el and then
-; modified in doom-highlight-non-default-indentation-h (in core/core-ui.el).
-; This is added to after-change-major-mode-hook in doom-init-ui-h (in
-; core/core-ui.el) and called a LOT: so I need to capture doom's modified
-; settings after that. The trouble is, this file (config.el) is called _before_
-; doom-init-ui-h which is called in window-setup-hook as the last gasp of
-; doom-initialize! find-file-hook appears to work.
+                                        ; whitespace-style etc are set up with default-values in whitespace.el and then
+                                        ; modified in doom-highlight-non-default-indentation-h (in core/core-ui.el).
+                                        ; This is added to after-change-major-mode-hook in doom-init-ui-h (in
+                                        ; core/core-ui.el) and called a LOT: so I need to capture doom's modified
+                                        ; settings after that. The trouble is, this file (config.el) is called _before_
+                                        ; doom-init-ui-h which is called in window-setup-hook as the last gasp of
+                                        ; doom-initialize! find-file-hook appears to work.
 (add-hook 'find-file-hook #'anr/set-whitespace-defaults 'append)
 
-; doom=>default=>off=>doom=>...
+                                        ; doom=>default=>off=>doom=>...
 (defun anr/toggle-whitespace () (interactive)
        (cond ((equal anr/whitespace-mode "doom")
               (setq whitespace-style anr/default-whitespace-style
@@ -266,48 +266,52 @@
 
 (global-set-key (kbd "C-<f4>") 'anr/toggle-whitespace)
 
+;; fasd
 (use-package! fasd
               :config
               (map! :leader
                     "fas" #'fasd-find-file)
               (global-fasd-mode +1))
 
+;; ace window
 (use-package! ace-window
               :config
               (map! :leader
                     "wM" #'ace-swap-window))
 
+;; lsp mode
 (after! lsp-mode
-  ;; stop formatting while I type
-  (setq lsp-enable-on-type-formatting nil)
-  ;; disable lsp-enable-indentation, too "electric" behavior
-  ;; where also following lines are affected
-  (setq lsp-enable-indentation nil)
-  ;; watching large projects may cause Emacs slow-down
-  (setq lsp-enable-file-watchers nil)
-  ;; disable
-  (setq lsp-lens-enable nil)
-  ;(setq lsp-diagnostics-provider :none)
-  ;(setq lsp-ui-sideline-enable nil)
-  )
+        ;; stop formatting while I type
+        (setq lsp-enable-on-type-formatting nil)
+        ;; disable lsp-enable-indentation, too "electric" behavior
+        ;; where also following lines are affected
+        (setq lsp-enable-indentation nil)
+        ;; watching large projects may cause Emacs slow-down
+        (setq lsp-enable-file-watchers nil)
+        ;; disable
+        (setq lsp-lens-enable nil)
+                                        ;(setq lsp-diagnostics-provider :none)
+                                        ;(setq lsp-ui-sideline-enable nil)
+        )
 
+;; cscope
 (setq consult-cscope-use-initial t)
 (use-package! consult-cscope
-  :defer t
-  :commands (consult-cscope-symbol
-             consult-cscope-definition
-             consult-cscope-called-by
-             consult-cscope-calling
-             consult-cscope-text
-             consult-cscope-egrep
-             consult-cscope-file
-             consult-cscope-including
-             consult-cscope-assignment))
+              :defer t
+              :commands (consult-cscope-symbol
+                         consult-cscope-definition
+                         consult-cscope-called-by
+                         consult-cscope-calling
+                         consult-cscope-text
+                         consult-cscope-egrep
+                         consult-cscope-file
+                         consult-cscope-including
+                         consult-cscope-assignment))
 
 (map! :leader
- (:prefix-map ("c" . "code")
-  (:prefix ("h" . "cscope")
-   :desc "Search symbol" "s" #'consult-cscope-symbol
-   :desc "Search definition" "d" #'consult-cscope-definition
-   :desc "Search text" "t" #'consult-cscope-text
-   :desc "Search file" "f" #'consult-cscope-file)))
+      (:prefix-map ("c" . "code")
+                   (:prefix ("h" . "cscope")
+                            :desc "Search symbol" "s" #'consult-cscope-symbol
+                            :desc "Search definition" "d" #'consult-cscope-definition
+                            :desc "Search text" "t" #'consult-cscope-text
+                            :desc "Search file" "f" #'consult-cscope-file)))
