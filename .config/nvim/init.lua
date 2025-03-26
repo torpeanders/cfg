38bd8724ab1857f544cf2379191acf92d89c71fa
Anders Rønningen <anders@ronningen.priv.no>
2025-03-26 20:17:21 +0100

    nvim: modernize

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,6 +71,7 @@ vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true,
 vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fl", ":Telescope current_buffer_fuzzy_find<CR>", { noremap = true, silent = true })
 
 -- Git Signs
 require("gitsigns").setup()
