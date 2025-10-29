4900b0071a3f4a53f1aa2bf4da5163d3dd5b3831
Anders Rønningen <anders.ronningen@neat.no>
2025-10-29 15:11:34 +0100

    doom: remove whitespace thingy

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -248,26 +248,6 @@
       :map evil-insert-state-map
       "C-x C-s" #'save-buffer)
 
-;; alternate whitespace-mode with whitespace.el defaults, doom defaults and off:
-(defun anr/set-whitespace-defaults()
-                                        ; only save the values the first time we get here
-  (unless (boundp 'anr/default-whitespace-style)
-    (setq anr/default-whitespace-style                (default-value 'whitespace-style)
-          anr/default-whitespace-display-mappings     (default-value 'whitespace-display-mappings)
-          anr/doom-whitespace-style                   whitespace-style
-          anr/doom-whitespace-display-mappings        whitespace-display-mappings
-          anr/whitespace-mode                         "doom")))
-
-                                        ; whitespace-style etc are set up with default-values in whitespace.el and then
-                                        ; modified in doom-highlight-non-default-indentation-h (in core/core-ui.el).
-                                        ; This is added to after-change-major-mode-hook in doom-init-ui-h (in
-                                        ; core/core-ui.el) and called a LOT: so I need to capture doom's modified
-                                        ; settings after that. The trouble is, this file (config.el) is called _before_
-                                        ; doom-init-ui-h which is called in window-setup-hook as the last gasp of
-                                        ; doom-initialize! find-file-hook appears to work.
-(add-hook 'find-file-hook #'anr/set-whitespace-defaults 'append)
-
-                                        ; doom=>default=>off=>doom=>...
 (defun anr/toggle-whitespace () (interactive)
        (cond ((equal anr/whitespace-mode "doom")
               (setq whitespace-style anr/default-whitespace-style
